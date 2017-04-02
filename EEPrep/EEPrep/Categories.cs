51800bc921a7bca72064b5a2cfecdd13d0c282b9
13OT <A.Alathel93@gmail.com>
2017-04-01 20:49:25 -0500

    Update Categories.cs

--- EEPrep/EEPrep/Categories.cs
+++ EEPrep/EEPrep/Categories.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,63 @@ using System.Windows.Forms;
 
 namespace EEPrep
 {
-    public partial class Categories : Form
+    public partial class CategoryForm : Form
     {
-        public Categories()
+        ArrayList Topics = new ArrayList();
+        int questions;
+        bool click = false;
+        public CategoryForm()
         {
             InitializeComponent();
         }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            click = true;
+            foreach(CheckBox CB in TopicGroupBox.Controls)
+            {
+               
+                if(CB.Checked)
+                {
+                    Topics.Add(CB.Text);
+                }
+                
+            }
+            foreach(CheckBox question in randomGrpBx.Controls)
+            {
+                if (question.Checked)
+                    questions = Convert.ToInt32(question.Tag);
+            }
+
+            this.Close();
+
+        }
+
+        private void checkBox9_CheckedChanged(object sender, EventArgs e)
+        {
+
+            checkBox5.Checked = !checkBox9.Checked;
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox9.Checked = !checkBox5.Checked;
+
+
+        }
+        public ArrayList GetTopics()
+        {
+            return Topics;
+        }
+        public int QNum ()
+        {
+            return questions;
+ 
+        }
+        public bool done()
+        {
+            return click;
+        }
+
     }
 }
