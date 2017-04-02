d74432d7def5fe3d977cf3d070ea71e2362c14d2
13OT <A.Alathel93@gmail.com>
2017-04-01 20:48:44 -0500

    Update Student.cs

--- EEPrep/EEPrep/Student.cs
+++ EEPrep/EEPrep/Student.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +9,68 @@ namespace EEPrep
 {
     public class Student
     {
-        private string email { set; get; }
-        private string name { set; get; }
-        private int numQuestionsCorrect { set; get; }
-        private int numQuestionsAttempt { set; get; }
-        private ArrayList topics { set; get; }
+        private string email;
+        private string name;
+        private int numQuestionsCorrect;
+        private int numQuestionsAttempt;
+        private ArrayList topics;
+        public Student()
+        {
+            Email = Name = "";
+            Questions_Correct = Questions_Attempted = 0;
+            topics = new ArrayList();
+        }
+        public string Email
+        {
+            get
+            {
+                return email ;
+            }
+
+            set
+            {
+                 email = value;
+            }
+        }
+        public string Name 
+        {
+            get
+            {
+                return name ;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+            public int Questions_Correct
+        {
+            get
+            {
+                return numQuestionsCorrect;
+            }
+
+            set
+            {
+                numQuestionsCorrect = value;
+            }
+        }
+            public int Questions_Attempted
+        {
+            get
+            {
+                return numQuestionsAttempt;
+            }
+
+            set
+            {
+                numQuestionsAttempt = value;
+            }
+        }
+        public void  setTopics(ArrayList listoftopics)
+        {
+            topics.AddRange(listoftopics);
+        }
     }
 }
